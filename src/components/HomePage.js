8de1d5c2d5571e8c16b231a889abb59851cc8fc7
jeffertan <jeffertan@outlook.com>
2019-07-26 23:33:33 +1000

    Study React

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return(
+        <div className = "jumbotron">
+            <h1>Pluralsight Administration</h1>
+            <p>React Flux, and React Router for ultra-responsive Web apps.</p>
+        </div>
+    );
+}
+
+export default HomePage;
